a16ca5b43a44f330d9640e69b741dcb0c467c892
yburkul <yburkul123@gmail.com>
2022-03-24 12:50:46 +0530

    Added:Linked List

---
+++ DataStructure/DataStructure/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkList is empty");
+            }
+            else
+            {
+                Console.WriteLine("LinkList as follows: ");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write("->");
+                }
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.Display();
+        }
+    }
+}
